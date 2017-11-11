aaee7af654f640e303b3feccd5593382c038e877
Bodor Gergely <bodorgergely91@gmail.com>
2017-11-11 21:29:54 +0100

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { passwordValidator, PasswordValidatorOptions } from './password-validator.directive';
+import { matchOtherValidator } from './match-other-validator.directive';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+
+  public passwordForm: FormGroup;
+  public defaultTextInputMinLength = 3;
+  public defaultTextInputMaxLength = 10;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initPasswordForm();
+  }
+
+  public submit(): void {
+    console.log(this.passwordForm.value);
+  }
+
+  private initPasswordForm(): void {
+    this.passwordForm = this.formBuilder.group({
+      passwords: this.formBuilder.group({
+        password: ['', [
+          Validators.required,
+          passwordValidator({ minLength: this.defaultTextInputMinLength, maxLength: this.defaultTextInputMaxLength })
+        ]],
+        confirmPassword: ['', [
+          Validators.required,
+          passwordValidator({ minLength: this.defaultTextInputMinLength, maxLength: this.defaultTextInputMaxLength }),
+          matchOtherValidator('password')
+        ]]
+      })
+    });
+  }
+
 }
